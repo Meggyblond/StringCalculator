89f0f4e026d654d25a57b4ffab439135e7fbf2a5
Megg <arnimegg@gmail.com>
2018-10-09 16:23:06 +0000

    Added functionality for when the input is an empty strings the function returns 0

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,5 +1,8 @@
 function add (numbers) {
-    
+    if(numbers == "") {
+	return 0;
+    }    
+
     var array = numbers.split(",");
     var sum = 0;
     for(var i = 0; i < array.length; i++) {
--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -3,3 +3,7 @@ const add = require("./StringCalculator"); //Import our calculator
 it("should return the sum of multiple numbers", () => {
     expect(add("0,1,2")).toBe(3);
 });
+
+it("should return 0 if the input is an empty string", () => {
+    expect(add("")).toBe(0);
+});
