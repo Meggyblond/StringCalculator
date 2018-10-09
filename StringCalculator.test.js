5b658761e6d2585209cde14692e83d9b6be559f0
Megg <arnimegg@gmail.com>
2018-10-09 16:29:41 +0000

    Added a test for one letter inputs

--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -7,3 +7,7 @@ it("should return the sum of multiple numbers", () => {
 it("should return 0 if the input is an empty string", () => {
     expect(add("")).toBe(0);
 });
+
+it("should return 1 if the input is 1", () => {
+    expect(add("1")).toBe(1);
+});
