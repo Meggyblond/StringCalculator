100bb3ee43b88e87d90ca449d0e16854ca09f531
Megg <arnimegg@gmail.com>
2018-10-09 16:51:56 +0000

    Added 3 more test for 2, 6 and 11 numbers. The function worked nicely

--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -11,3 +11,15 @@ it("should return 0 if the input is an empty string", () => {
 it("should return 1 if the input is 1", () => {
     expect(add("1")).toBe(1);
 });
+
+it("should return the sum of multiple numbers", () => {
+    expect(add("0,1,2,3,4,5")).toBe(15);
+});
+
+it("should return the sum of multiple numbers", () => {
+    expect(add("0,1")).toBe(1);
+});
+
+it("should return the sum of multiple numbers", () => {
+    expect(add("0,1,2,3,4,5,6,7,8,9,10")).toBe(55);
+});
